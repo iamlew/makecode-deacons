8531b7792cd601b88c429998366d9d81f0b8932c
Loren Whitney <loren.whitney@gmail.com>
2020-02-19 13:04:13 -0700

    first commit - blinky

---
+++ blinky.ts
@@ -0,0 +1,10 @@
+loops.forever(function () {
+    light.showRing(
+    `red red red red red red red red red red`
+    )
+    loops.pause(500)
+    light.showRing(
+    `black black black black black black black black black black`
+    )
+    loops.pause(500)
+})
